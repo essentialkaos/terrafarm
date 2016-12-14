e4ee15ee3c9576546a2ee6048744657ec10d9868
Anton Novojilov <andy@essentialkaos.com>
2016-12-14 15:26:58 -0500

    Added template for centos6+centos7

---
+++ terradata/c6-c7/builder-c6-x64.tf
@@ -0,0 +1,54 @@
+resource "digitalocean_droplet" "builderc-c6-x64" {
+  image = "centos-6-5-x64"
+  name = "terrafarm-c6-x64"
+  region = "${var.region}"
+  size = "${var.node_size}"
+  ssh_keys = [
+    "${var.fingerprint}"
+  ]
+
+  connection {
+    user = "root"
+    type = "ssh"
+    private_key = "${file(var.key)}"
+    timeout = "2m"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "export PATH=$PATH:/usr/bin",
+      "echo 'Cleaning yum cache...'",
+      "yum -y -q clean expire-cache",
+      "echo 'Updating system packages...'",
+      "yum -y -q update",
+      "echo 'Installing KAOS repository package...'",
+      "yum -y -q install https://yum.kaos.io/6/release/i386/kaos-repo-7.2-0.el6.noarch.rpm",
+      "echo 'Installing EPEL repository package...'",
+      "yum -y -q install epel-repo",
+      "echo 'Updating packages...'",
+      "yum -y -q update",
+      "echo 'Installing RPMBuilder Node package...'",
+      "yum -y -q install rpmbuilder-node",
+      "echo 'Starting node configuration...'",
+      "yum-config-manager --disable kaos-release &> /dev/null",
+      "yum-config-manager --enable kaos-release-x64 &> /dev/null",
+      "sed -i 's#builder:!!#builder:${var.auth}#' /etc/shadow",
+      "echo 'Build node configuration complete'"
+    ]
+  }
+
+  provisioner "file" {
+    source = "conf/hosts.allow"
+    destination = "/etc/hosts.allow"
+  }
+
+  provisioner "file" {
+    source = "conf/c6-rpmmacros"
+    destination = "/home/builder/.rpmmacros"
+  }
+
+  provisioner "file" {
+    source = "conf/sudoers"
+    destination = "/etc/sudoers"
+  }
+}
---
+++ terradata/c6-c7/builder-c7-x64.tf
@@ -0,0 +1,50 @@
+resource "digitalocean_droplet" "builder-c7-x64" {
+  image = "centos-7-0-x64"
+  name = "terrafarm-c7-x64"
+  region = "${var.region}"
+  size = "${var.node_size}"
+  ssh_keys = [
+    "${var.fingerprint}"
+  ]
+
+  connection {
+    user = "root"
+    type = "ssh"
+    private_key = "${file(var.key)}"
+    timeout = "2m"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "export PATH=$PATH:/usr/bin",
+      "echo 'Cleaning yum cache...'",
+      "yum -y -q clean expire-cache",
+      "echo 'Updating system packages...'",
+      "yum -y -q update",
+      "echo 'Installing KAOS repository package...'",
+      "yum -y -q install https://yum.kaos.io/7/release/x86_64/kaos-repo-7.2-0.el7.noarch.rpm",
+      "echo 'Updating packages...'",
+      "yum -y -q update",
+      "echo 'Installing RPMBuilder Node package...'",
+      "yum -y -q install rpmbuilder-node",
+      "echo 'Starting node configuration...'",
+      "sed -i 's#builder:!!#builder:${var.auth}#' /etc/shadow",
+      "echo 'Build node configuration complete'"
+    ]
+  }
+
+  provisioner "file" {
+    source = "conf/hosts.allow"
+    destination = "/etc/hosts.allow"
+  }
+
+  provisioner "file" {
+    source = "conf/c7-rpmmacros"
+    destination = "/home/builder/.rpmmacros"
+  }
+
+  provisioner "file" {
+    source = "conf/sudoers"
+    destination = "/etc/sudoers"
+  }
+}
